437670554c42ee2dcdded865b92b00dab193b4e7
Brian Low <github@brianlow.com>
2022-08-08 09:41:49 -0600

    Remove TAPPING defines

--- keyboards/sofle_choc/config.h
+++ keyboards/sofle_choc/config.h
@@ -35,7 +35,6 @@
 #define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
 #define DIODE_DIRECTION COL2ROW
 
-#define TAPPING_TERM 100
 #define DEBOUNCE 5
 
 // Encoder support
@@ -44,8 +43,6 @@
 #define ENCODERS_PAD_A_RIGHT { F4 }
 #define ENCODERS_PAD_B_RIGHT { F5 }
 #define ENCODER_RESOLUTION 2
-
-#define TAP_CODE_DELAY 10
 
 // Communication between sides
 #define USE_SERIAL
